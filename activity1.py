3ebe5fc77805e2caf4a3552300d5f1be464d7aaa
Reda452 <redasakhi0@gmail.com>
2024-12-24 19:22:23 +0100

    new

---
+++ activity1.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age"))
+if age>=10:
+    if age<=20:
+        print("Your age is : ", age, " \n You are not allowed to enter the class")
+    else:
+        print ("Your age is:",age, " \nYou are not allowed to enter the class")
+else:
+    print("Your age is : ", age, " \n You are not eligible")
